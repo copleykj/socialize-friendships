f6f5c6100f566fbfc447269fb4b7af59b8669ac2
Kelly <copleykj@gmail.com>
2015-04-03 21:14:58 -0400

    depend on new user-model and base-model

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: "socialize:friendships",
     summary: "A social friendship package",
-    version: "0.3.0",
+    version: "0.3.1",
 });
 
 Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "meteor", "mongo", "underscore", "socialize:user-model@0.1.0", "socialize:base-model@0.1.0",
+        "meteor", "mongo", "underscore", "socialize:user-model@0.1.1", "socialize:base-model@0.1.2",
         "copleykj:simple-publish@1.0.4", "aldeed:simple-schema@1.3.0", "aldeed:collection2@2.3.2",
         "matb33:collection-hooks@0.7.9", "meteorhacks:unblock@1.1.0"
     ]);
