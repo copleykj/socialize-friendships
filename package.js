55f1f156e7a6a639a919222ee4b1c1545d9c971b
Kelly <copleykj@gmail.com>
2015-06-01 13:43:36 -0400

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:friendships",
     summary: "A social friendship package",
-    version: "0.3.3",
+    version: "0.3.4",
 });
 
 Package.onUse(function(api) {
