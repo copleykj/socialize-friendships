d87e3553fdb3ec4556b77e8edc43014264c8be1b
Kelly <copleykj@gmail.com>
2015-06-22 11:44:36 -0400

    filter requests and pending requests for denied and ignored.. Add publish for ignored

--- request-model/server/publications.js
+++ request-model/server/publications.js
@@ -11,7 +11,31 @@ Meteor.publish('friendRequests', function(options){
     Meteor.publishWithRelations({
         handle: this,
         collection: Meteor.requests,
-        filter: {userId:this.userId},
+        filter: {userId:this.userId, denied:{$exists:false}, ignored:{$exists:false}},
+        options:options,
+        mappings: [{
+            key: 'requesterId',
+            collection: Meteor.users,
+            options:{fields:{username:true, avatarId:true}}
+        }]
+    });
+
+});
+
+Meteor.publish('ignoredFriendRequests', function(options){
+    if(!this.userId){
+        return this.ready();
+    }
+
+    options = options || {};
+
+    //only allow the limit and skip options
+    options = _.pick(options, "limit", "skip", "sort");
+
+    Meteor.publishWithRelations({
+        handle: this,
+        collection: Meteor.requests,
+        filter: {userId:this.userId, denied:{$exists:false}, ignored:{$exists:true}},
         options:options,
         mappings: [{
             key: 'requesterId',
@@ -35,7 +59,7 @@ Meteor.publish('outgoingFriendRequests', function(options){
     Meteor.publishWithRelations({
         handle: this,
         collection: Meteor.requests,
-        filter: {requesterId:this.userId},
+        filter: {requesterId:this.userId, denied:{$exists:false}},
         options:options,
         mappings: [{
             key: 'requesterId',
