c2396d4d20f2181149f109ba9990dc35172bae8b
Kelly <copleykj@gmail.com>
2015-06-30 12:37:40 -0400

    update user-model version dependency

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
     api.versionsFrom("1.0.2.1");
 
     api.use([
-        "socialize:user-model@0.1.3"
+        "socialize:user-model@0.1.4"
     ]);
 
     api.imply("socialize:user-model");
