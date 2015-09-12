ebb947c5b068d78f483e7e2f29b963f80a112a67
copleykj <copleykj@gmail.com>
2015-09-12 16:39:20 -0400

    patch version bump 0.4.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "socialize:friendships",
     summary: "A social friendship package",
-    version: "0.4.0",
+    version: "0.4.1",
     git: "https://github.com/copleykj/socialize-friendships.git"
 });
 
