f93cfc23ab0c1ff602ed28c3083562c1e9b3e5ad
Kelly Copley <copleykj@gmail.com>
2017-07-12 21:34:22 -0400

    Requests are linked to users so pass User model to onAccepted

--- common/request-hooks.js
+++ common/request-hooks.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-unresolved */
 import { Meteor } from 'meteor/meteor';
 import { Request } from 'meteor/socialize:requestable';
+import { User } from 'meteor/socialize:user-model';
 /* eslint-enable import/no-unresolved */
 
 import { Friend } from './friend-model.js';
 
-Request.onAccepted(Friend, function onAcceptedHook() {
+Request.onAccepted(User, function onAcceptedHook() {
     if (this.type === 'friend') {
         new Friend({ userId: Meteor.userId(), friendId: this.requesterId }).save();
     }
