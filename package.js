efba8391e824790f7f7e871ac3d072d160e670bd
Kelly Copley <copleykj@gmail.com>
2018-05-26 22:53:12 -0400

    bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
     name: 'socialize:friendships',
     summary: 'A social friendship package',
-    version: '1.0.1',
+    version: '1.0.2',
     git: 'https://github.com/copleykj/socialize-friendships.git',
 });
 
