29d0a2f5e4d0bd70d3ea02ebcb241987a87ceb08
Kelly Copley <copleykj@gmail.com>
2020-06-27 13:37:30 -0400

    bump publish-composite

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.onUse(function _(api) {
 
     api.use([
         'check',
-        'reywood:publish-composite@1.7.2',
+        'reywood:publish-composite@1.7.3',
         'socialize:user-blocking@1.0.1',
         'socialize:requestable@1.0.3',
     ]);
